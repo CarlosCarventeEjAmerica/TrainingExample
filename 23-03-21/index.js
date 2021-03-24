4522b8dae9e859235268468811dd8cab8b38a245
CarlosCarventeEjAmerica <81254158+CarlosCarventeEjAmerica@users.noreply.github.com>
2021-03-24 10:46:15 -0600

    JavaScript walk thru

---
+++ 23-03-21/index.js
@@ -0,0 +1,17 @@
+let a = 'Hello World!';
+
+
+//normal function
+function b(){
+    return 'Bye!'
+}
+
+// //arrow funtion
+// const b1 = () => {
+//     return 'Bye!'
+// }
+
+// //function expression
+// const b2 = function(){
+
+// }
