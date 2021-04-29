81ab7e56b690ae7fd2c6af324cfe098264f0f0ee
CarlosCarventeEjAmerica <81254158+CarlosCarventeEjAmerica@users.noreply.github.com>
2021-04-29 14:12:22 -0500

    Add User.entity, post.entity, and work with postgresDB

---
+++ 29-04-2021/src/routes/posts.ts
@@ -0,0 +1,57 @@
+import express from 'express';
+import {getRepository} from 'typeorm';
+
+import Posts from '../models/post.entity';
+
+let postRouter = express.Router();
+
+/* Posts Data - Use data from jsonPlaceHolder */
+let data = {
+    posts: [
+        {id: 1 , userId: 1, title: 'sunt aut facere', body: 'quia et suscipit suscipit recusandae consequuntur expedita'},
+        {id: 2 , userId: 1, title: 'qui est esse', body: 'est rerum tempore vitae sequi sint nihil reprehenderit dolor'},
+        {id: 3 , userId: 1, title: 'ea molestias quasi exercitationem', body: 'et iusto sed quo iure voluptatem occaecati omnis'}
+    ]
+}
+
+/* Implement Methods */
+postRouter.get('/', async (req, res) => {
+    /* res.json(data.posts); */
+    let [post, error] =  await handleAsync( getRepository(Posts).find());
+    if (error) return res.send(error);
+    res.send(post);
+});
+
+postRouter.get('/:id', (req, res) => {
+    let post = data.posts.filter( item => item.id.toString() === req.params.id);
+    res.json(post[0]);
+});
+
+
+
+postRouter.post('/', async (req, res) => {
+    /* res.json('Creating'); */
+    let data = req.body;
+    const newPost = getRepository(Posts).create(data);
+    let [post, error] =  await handleAsync( getRepository(Posts).save(newPost));
+
+    if (error) return res.send(error);
+    res.send(post);
+
+});
+
+postRouter.patch('/', (req, res) => {
+    res.json('Patching....');
+});
+
+postRouter.delete('/', (req, res) => {
+    res.json('Deleting....');
+});
+
+const handleAsync = (promise: Promise<any>) =>{
+    return promise
+        .then( (data: any) => ([data, null]) )
+        .catch( (error: any) => ([null, error]) )
+}
+
+export default postRouter;
